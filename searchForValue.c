0dcd95619755d2a26cd521a4f1304baf1d92513c
Ramzi Khantouchi <47026044+ramzikhantouchi@users.noreply.github.com>
2023-12-02 14:52:58 +0100

    Add files via upload

---
+++ searchForValue.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+
+    int length = 20, value;
+    int Array[length];
+    
+    printf("Enter an integer number: ");
+    scanf("%d",&value);
+
+    for (int i = 0; i<length; i++){
+        if(Array[i] == value){
+            printf("the number %d exist at the position %d.\n", value, i);
+            break;
+        }
+        else if(i == (length-1)){
+            printf("The number %d does not exist in the array.\n",value);
+        }
+    }
+
+    return 0;
+}
